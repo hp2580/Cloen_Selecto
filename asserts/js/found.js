a295dd487748cd39d7ef3dbd8e950d4972b64ac0
sumin <happycandy0909@gamil.com>
2022-12-21 19:50:20 +0900

    Page Found header, aside end

---
+++ asserts/js/found.js
@@ -0,0 +1,59 @@
+let menus = document.querySelectorAll(".h_menu a");
+let depth2 = document.querySelector(".depth2");
+let header = document.querySelector("header");
+let side_menus = document.querySelectorAll(".menu a");
+
+window.onload = () => {
+  setTimeout(() => {
+    header.classList.add("show");
+  });
+};
+
+window.onscroll = () => {
+  let currentY = window.scrollY;
+  let direction = currentY - prevY;
+  if (window.scrollY > 0) {
+    if (direction < 0) header.classList.add("show");
+    else {
+      header.classList.remove("show");
+      depth2.classList.remove("show");
+      document.querySelector(".h_found").classList.remove("convert");
+      document.querySelector(".found").classList.remove("convert");
+    }
+  } else header.classList.add("show");
+  prevY = currentY;
+};
+
+document.body.addEventListener("mousemove", function (e) {
+  let cursor = document.querySelector(".cursor");
+  cursor.style.top = `${e.pageY}px`;
+  cursor.style.left = `${e.pageX}px`;
+});
+
+document.querySelector(".btn_side").addEventListener("click", () => {
+  document.querySelector(".side_menu").classList.add("active");
+});
+
+document.querySelector(".btn_close").addEventListener("click", () => {
+  document.querySelector(".side_menu").classList.remove("active");
+});
+
+for (let side_menu of side_menus) {
+  side_menu.addEventListener("click", function (e) {
+    e.preventDefault();
+    let wrap = this.parentElement.parentElement;
+    wrap.classList.toggle("active");
+  });
+}
+
+for (let menu of menus) {
+  menu.addEventListener("mouseenter", () => {
+    depth2.classList.add("show");
+    document.querySelector(".h_found").classList.add("convert");
+  });
+}
+
+header.addEventListener("mouseleave", () => {
+  depth2.classList.remove("show");
+  document.querySelector(".h_found").classList.remove("convert");
+});
